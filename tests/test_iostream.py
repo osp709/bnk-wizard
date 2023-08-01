481ebb3f07436be561e7fc16d19bf880d403fae8
Om Shri Prasathm <omshriprasath@gmail.com>
2023-08-01 22:03:51 +0530

    Added tests

---
+++ tests/test_iostream.py
@@ -0,0 +1,29 @@
+"""
+Tests for IOStream class
+"""
+
+from src.iostream import IOStream
+
+
+def test_read_bytes():
+    """
+    Test for read_bytes
+    """
+    inp = IOStream("data/hello.txt")
+    assert inp.read_bytes(-1) == "Hello World!".encode()
+
+
+def test_read_string():
+    """
+    Test for read_bytes
+    """
+    inp = IOStream("data/hello.txt")
+    assert inp.read_str(len("Hello World!")) == "Hello World!"
+
+
+def test_read_int():
+    """
+    Test for read_bytes
+    """
+    inp = IOStream("data/hello.txt")
+    assert inp.read_int() == 1214606444
