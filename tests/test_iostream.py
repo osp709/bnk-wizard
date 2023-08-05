02c643a8244cedefee1c23496ed75fd7f44d1dd8
Om Shri Prasathm <omshriprasath@gmail.com>
2023-08-05 16:31:19 +0530

    changed text file outputs

--- tests/test_iostream.py
+++ tests/test_iostream.py
@@ -10,9 +10,9 @@ def test_read_bytes():
     Test for read_bytes
     """
     res = "Hello World!".encode()
-    with open("data/hello.txt", "wb") as f:
+    with open("data/hello", "wb") as f:
         f.write(res)
-    inp = InputStream("data/hello.txt")
+    inp = InputStream("data/hello")
     assert inp.read_bytes(-1) == res
     inp.close()
 
@@ -22,9 +22,9 @@ def test_read_string():
     Test for read_string
     """
     res = "Hello World!"
-    with open("data/hello.txt", "w") as f:
+    with open("data/hello", "w") as f:
         f.write(res)
-    inp = InputStream("data/hello.txt")
+    inp = InputStream("data/hello")
     assert inp.read_str(len("Hello World!")) == res
     inp.close()
 
@@ -34,9 +34,9 @@ def test_read_int():
     Test for read_int
     """
     res = 12
-    with open("data/hello.txt", "wb") as f:
+    with open("data/hello", "wb") as f:
         f.write(res.to_bytes(length=4, byteorder="little"))
-    inp = InputStream("data/hello.txt")
+    inp = InputStream("data/hello")
     assert inp.read_int() == res
     inp.close()
 
@@ -45,11 +45,11 @@ def test_write_bytes():
     """
     Test for write_bytes
     """
-    out = OutputStream("data/bye.txt")
+    out = OutputStream("data/bye")
     res = "Bye World!".encode()
     out.write_bytes(res)
     out.close()
-    with open("data/bye.txt", "rb") as f:
+    with open("data/bye", "rb") as f:
         assert f.read() == res
 
 
@@ -57,11 +57,11 @@ def test_write_string():
     """
     Test for write_string
     """
-    out = OutputStream("data/bye.txt")
+    out = OutputStream("data/bye")
     res = "Bye World!"
     out.write_str(res)
     out.close()
-    with open("data/bye.txt", "rb") as f:
+    with open("data/bye", "rb") as f:
         assert f.read().decode() == res
 
 
@@ -69,9 +69,9 @@ def test_write_int():
     """
     Test for write_int
     """
-    out = OutputStream("data/bye.txt")
+    out = OutputStream("data/bye")
     res = 123
     out.write_int(res)
     out.close()
-    with open("data/bye.txt", "rb") as f:
+    with open("data/bye", "rb") as f:
         assert int.from_bytes(f.read(), "little") == res
